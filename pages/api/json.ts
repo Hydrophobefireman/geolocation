10f292a5716d90bdd32acf878dede7376e8f45e0
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2022-07-19 21:00:12 +0530

    fix

--- pages/api/json.ts
+++ pages/api/json.ts
@@ -1,11 +1,30 @@
+import type {NextRequest} from "next/server";
+
+import countries from "./lib/_countries.json";
+
 export const config = {
   runtime: "experimental-edge",
 };
-export default function (req: Request) {
+export default function handle(req: NextRequest) {
+  const {geo} = req;
+  const country = geo.country || "US";
+  const city = geo.city || "San Francisco";
+  const region = geo.region || "CA";
+
+  const countryInfo = countries.find((x) => x.cca2 === country);
+
+  const currencyCode = Object.keys(countryInfo.currencies)[0];
+  const currency = countryInfo.currencies[currencyCode];
+  const languages = Object.values(countryInfo.languages).join(", ");
+
   return new Response(
     JSON.stringify({
-      geo: (req as any).geo,
-      name: "Jim Halpert",
+      country,
+      city,
+      region,
+      currencyCode,
+      currency,
+      languages,
     }),
     {
       status: 200,
--- scripts/countries.js
+++ scripts/countries.js
@@ -1,35 +1,37 @@
-const { join } = require('path')
-const { writeFile } = require('fs/promises')
+const {join} = require("path");
+const {writeFile} = require("fs/promises");
+const {write} = require("fs");
 
 const JSON_URL =
-  'https://raw.githubusercontent.com/mledoze/countries/master/dist/countries.json'
+  "https://raw.githubusercontent.com/mledoze/countries/master/dist/countries.json";
 
 async function setupCountryInfo() {
-  const req = await fetch(JSON_URL)
-  const countriesData = await req.json()
+  const req = await fetch(JSON_URL);
+  const countriesData = await req.json();
 
   // grab only the country data we need
   const data = countriesData.map((x) => ({
     cca2: x.cca2,
     currencies: x.currencies,
     languages: x.languages,
-  }))
+  }));
 
-  const filePath = join(process.cwd(), 'lib/countries.json')
-  const content = JSON.stringify(data)
+  const filePath = join(process.cwd(), "lib/countries.json");
+  const content = JSON.stringify(data);
 
-  await writeFile(filePath, content)
+  await writeFile(filePath, content);
+  await write(join(process.cwd(), "pages/api/lib/_countries.json"), content);
 }
 
 function withCountryInfo(nextConfig = {}) {
-  const { rewrites } = nextConfig
+  const {rewrites} = nextConfig;
   // Not really adding rewrites but using its async behavior to load the country data
   nextConfig.rewrites = async (...args) => {
-    await setupCountryInfo()
-    return rewrites?.(...args) ?? {}
-  }
+    await setupCountryInfo();
+    return rewrites?.(...args) ?? {};
+  };
 
-  return nextConfig
+  return nextConfig;
 }
 
-module.exports = { withCountryInfo, setupCountryInfo }
+module.exports = {withCountryInfo, setupCountryInfo};
